623312f05abbf6e920ac73c128d456cf1abf166d
Alejandrogv2304 <alejandrogv.1011@gmail.com>
2025-07-16 15:39:01 -0500

    Añadiendo forms

---
+++ src/components/Forms/Form.tsx
@@ -0,0 +1,31 @@
+import { countries } from "../../data/countries";
+import styles from './Form.module.css'
+
+
+export default function Form() {
+  return (
+    <form className={styles.form}>
+        <div className={styles.field}>
+            <label htmlFor="city">Ciudad:</label>
+            <input
+            type="text"
+            id="city"
+            name="city"
+            placeholder="Ciudad"
+            />
+        </div>
+
+        <div className={styles.field}>
+            <label htmlFor="">País:</label>
+             <select>
+             <option value=''>-- Seleccione un país --</option>
+             {countries.map((country)=>(
+                <option value={country.code} key={country.code}>{country.name}</option>
+             ))}
+             </select>
+        </div>
+
+        <input className={styles.submit} type="submit" value='Consultar Clima'/>
+    </form>
+  )
+}
