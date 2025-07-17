4dc19f4bbd31dd7a4c74d76338a2885ff9c0d441
Alejandrogv2304 <alejandrogv.1011@gmail.com>
2025-07-16 19:16:41 -0500

    Agregando alerts

--- src/components/Forms/Form.tsx
+++ src/components/Forms/Form.tsx
@@ -1,10 +1,35 @@
+import { useState, type ChangeEvent, type FormEvent } from "react";
 import { countries } from "../../data/countries";
 import styles from './Form.module.css'
+import type { SearchType } from "../../types";
+import Alert from "../Alert/Alert";
 
 
 export default function Form() {
+  const [search, setSearch] = useState<SearchType>({
+    city: '',
+    country: ''
+  })
+
+  const [alert, setAlert] = useState('')
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement> ) =>{
+   setSearch({...search,
+    [e.target.name]: e.target.value
+   })
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) =>{
+   e.preventDefault()
+
+   if(Object.values(search).includes('')){
+    setAlert('Todos los campos son obligatorios')
+    return
+   }
+  }
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
+      {alert && <Alert>{alert}</Alert>}
         <div className={styles.field}>
             <label htmlFor="city">Ciudad:</label>
             <input
@@ -12,12 +37,19 @@ export default function Form() {
             id="city"
             name="city"
             placeholder="Ciudad"
+            value={search.city}
+            onChange={handleChange}
             />
         </div>
 
         <div className={styles.field}>
-            <label htmlFor="">País:</label>
-             <select>
+            <label htmlFor="country">País:</label>
+             <select
+             id="country"
+             value={search.country}
+             name="country"
+             onChange={handleChange}
+             >
              <option value=''>-- Seleccione un país --</option>
              {countries.map((country)=>(
                 <option value={country.code} key={country.code}>{country.name}</option>
